d7c902ee8c3a12c20938ebec1743340a70088e52
Lucas Spitzer <104842840+pangolino30@users.noreply.github.com>
2023-01-08 17:26:22 +0100

    Ajout du calcul de hauteur et de volume

--- code/code.ino
+++ code/code.ino
@@ -1,29 +1,48 @@
-#include <Wire.h>
-#include "rgb_lcd.h"            //Pour écran LCD
-#include <SoftwareSerial.h>     //Pour module Bluetooth
-SoftwareSerial bluetooth(2,3); 
+//#include <Wire.h>
+//#include "rgb_lcd.h"            //Pour écran LCD
+//#include <SoftwareSerial.h>     //Pour module Bluetooth
+//SoftwareSerial bluetooth(2,3); 
  
-rgb_lcd lcd;
+//rgb_lcd lcd;
 
-int vegaek21 = 0;   // Variable pour la valeur de la sonde
-long hauteur;
-float tension;
-float volume;
+int vegaek21 = 0;               // Variable pour la valeur de la sonde
+int hauteurEau = 0;
+int volumeEau = 0;
 
-void setup()
+float hauteurCuve = 64;
+float longueurCuve = 34;
+float largeurCuve = 34;
 
-{
-    lcd.begin(16, 2);          // Définit la résolution de l'écran LCD en 16x2
-    Serial.begin(9600);
-    bluetooth.begin(9600);
+void setup() {
+
+    //lcd.begin(16, 2);           // Définit la résolution de l'écran LCD en 16x2
+    Serial.begin(9600);           //
+    //bluetooth.begin(9600);      //
+
 }
 
-void loop()
+void loop() {
 
-{
     long hauteur;
     float tension;
     float volume;
-    valeur = analogRead(A0);     // Lit la valeur de la sonde sur le pin A0
+    vegaek21 = analogRead(A0);    // Lit la valeur de la sonde sur le pin A0
+    
+    // Calcul la hauteur de l'eau dans la cuve
+    hauteurEau = (vegaek21 * hauteurCuve) / 1024;
+    Serial.print("Hauteur eau: ");
+    Serial.print(hauteurEau);
+    Serial.println(" cm");
+
+    // Calcul le volume d'eau dans la cuve
+    volumeEau = hauteurEau * largeurCuve * longueurCuve;
+    Serial.println(hauteurEau);
+    Serial.println(longueurCuve);
+    Serial.println(largeurCuve);
+    Serial.print("Volume eau: ");
+    Serial.print(volumeEau);
+    Serial.println(" cm3");
+    
+    delay(2000);
 
 }
--- code/notes.md
+++ code/notes.md
@@ -1,10 +1,6 @@
 # Notes
 
-##
+## Informations
 
--   L'intensité de la sonde varie entre 20mA et 2A
-
--   20mA => 0%
--   2A => 100%
-
--   Comme la sonde risque d'être trop grande, 100% sera vers les 1.8A
+-   20mA => 0% => 0cm => 0
+-   2A => 100% => 64cm => 1024
